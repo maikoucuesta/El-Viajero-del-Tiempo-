aa066b1fa74d53f4ea7cfc2891254c6245e95f24
maikoucuesta <miguelcuestac2012@gmail.com>
2026-08-05 11:42:06 +0200

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.miguel.elviajero',
+  appName: 'El Viajero Del Tiempo',
+  webDir: 'www'
+};
+
+export default config;
