ffe747f2fa73f2cfdc2b928ab79da90726b3d988
maikoucuesta <miguelcuestac2012@gmail.com>
2026-08-05 09:19:11 +0200

    Delete el-viajero-apk-proyecto directory

--- el-viajero-apk-proyecto/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.miguel.elviajero',
-  appName: 'El Viajero Del Tiempo',
-  webDir: 'www'
-};
-
-export default config;
